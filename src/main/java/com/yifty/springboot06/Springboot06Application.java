c1ee68f2d654204482a958d04eadd49f43740972
Yifty <yifty08@icloud.com>
2018-07-16 19:11:44 -0400

    Initial commit

---
+++ src/main/java/com/yifty/springboot06/Springboot06Application.java
@@ -0,0 +1,12 @@
+package com.yifty.springboot06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot06Application.class, args);
+    }
+}
